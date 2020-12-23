34565b72a0dc9f4a6e4c7a40921ad9cdbfe97fe9
Aaron Muesing <muesing.music@gmail.com>
2020-12-23 22:31:18 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -22,3 +22,11 @@ def output_array(array)
   end
 end
 
+def return_string_array(array)
+  count = 0
+  while count < array.length do
+    array[count] = array[count].to_s
+    count += 1
+  end
+  array
+end
